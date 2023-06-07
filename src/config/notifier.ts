04e2985a5743d0b519c28b18b9691e6d02fbac4c
pyphilia <pyphilia@gmail.com>
2023-06-07 14:52:56 +0200

    refactor: update deploy dev refactor, fix apps query client

--- src/config/notifier.ts
+++ src/config/notifier.ts
@@ -8,7 +8,7 @@ import {
   UPLOAD_FILES_SUCCESS_MESSAGE,
 } from './messages';
 
-const { uploadFileRoutine } = ROUTINES;
+const { uploadAppDataFileRoutine } = ROUTINES;
 
 interface NotifierInt {
   type: string;
@@ -21,17 +21,17 @@ export default ({ type, payload }: NotifierInt): void => {
   let message = null;
   switch (type) {
     // error messages
-    case uploadFileRoutine.FAILURE: {
+    case uploadAppDataFileRoutine.FAILURE: {
       message = UPLOAD_FILES_ERROR_MESSAGE;
       break;
     }
-    case uploadFileRoutine.SUCCESS: {
+    case uploadAppDataFileRoutine.SUCCESS: {
       message = UPLOAD_FILES_SUCCESS_MESSAGE;
       break;
     }
     // progress messages
     // todo: this might be handled differently
-    case uploadFileRoutine.REQUEST: {
+    case uploadAppDataFileRoutine.REQUEST: {
       toast.info(i18n.t('UPLOAD_FILES_PROGRESS_MESSAGE'));
       break;
     }
